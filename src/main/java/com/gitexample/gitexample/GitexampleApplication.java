c403e91ec479edcdcef781d90f96e1c8e9d97526
amirsohel <sohailmandrupkar975@gmail.com>
2024-07-19 17:45:52 +0530

    first commit by me

---
+++ src/main/java/com/gitexample/gitexample/GitexampleApplication.java
@@ -0,0 +1,13 @@
+package com.gitexample.gitexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitexample/gitexample/GitexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitexample.gitexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
